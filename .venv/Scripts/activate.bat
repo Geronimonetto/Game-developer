bd21e48ac188e42fa08cd4fbbcedce8f8245e039
unknown <geronimomorais1617@gmail.com>
2024-08-11 17:17:27 -0300

    [UPDATE] -- Ajustando todas as pastas

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\geron\PycharmProjects\pythonProject\Engenheiro_de_dados\.venv
+set VIRTUAL_ENV=C:\Users\Geronimo\Documents\Estudos\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
